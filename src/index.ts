b8755563b0f028fe9917b5a1becc97135676002e
Ki <k.allegrain@gmail.com>
2023-10-08 19:10:57 +0200

    move comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import LcdScreen from "./screen/screen";
 
+// check the available space
+
 const columns: number = process.stdout.columns;
 const max_chars = Math.floor(columns / 3);
 const screen = new LcdScreen(max_chars);
 
 var args = process.argv[2];
-
+//check if args is a number
 if(!args || isNaN(Number(args))){
 	throw "Please provide a valid number to be printed. Usage : npm start -- <number>";
 }
 
-//check if args is a number
-
-
-// check the available space
 
 
 //everythiing is okay, print
