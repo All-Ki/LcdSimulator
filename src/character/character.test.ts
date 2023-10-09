f3f5b882d7152a055e294b15607451102e91d3a8
Ki <k.allegrain@gmail.com>
2023-10-09 14:36:57 +0200

    added good case scenario tests to characters

--- src/character/character.test.ts
+++ src/character/character.test.ts
@@ -4,9 +4,21 @@ describe("character specification", () => {
 	test("Should not accept characters longer than 1 character", () => {
 		expect(() => set_character("aa", "   ", "   ", "   ")).toThrow(E_INVALID_KEY);
 	})
-
-	test("Should only allow spaces, hyphens and underscores", () => {
+	test("Should not accept characters shorter than 1 character", () => {
+		expect(() => set_character("", "   ", "   ", "   ")).toThrow(E_INVALID_KEY);
+	})
+	
+	test("should accept when keys wiht one character", () => {
+		expect(() => set_character("a", "   ", "   ", "   ")).not.toThrow();
+		
+		
+	})
+	test("Should error when characters are other than spaces, hyphens and underscores", () => {
 		expect(() => set_character("a", "x  ", "   ", "   "))
 		.toThrow(E_BAD_SEGMENTS);
+	})
+	test("should accept when characters are spaces, hyphens and underscores", () => {
+		expect(() => set_character("a", "   " ,"  |", " __")).not.toThrow();
+	
 	})
 })
